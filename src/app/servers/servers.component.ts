fc33bfc82924cc0418d3ddad3dc20404b62dc13b
khaledasad <khaledabuasad94@gmail.com>
2018-11-09 10:48:47 -0500

    Data Binding 2 Way very Important

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -8,13 +8,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ServersComponent implements OnInit {
   allowNewServer = false;
+  serverCreationStatus = "No Server was created!";
+  serverName= "TestServer";
 
-  constructor() { 
+  constructor() 
+  { 
     setTimeout(() => {this.allowNewServer = true;} ,2000);
 
   }
+  
+  ngOnInit() 
+  {
+  }
 
-  ngOnInit() {
+  onCreateServer()
+  {
+    this.serverCreationStatus = "Server was created!";
+  }
+
+  onUpdateServerName(event : any)
+  {
+    this.serverName = (<HTMLInputElement>event.target).value;
   }
 
 }
